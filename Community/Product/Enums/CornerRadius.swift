c9a75ec3f48ee9a5c29127fd1a9e9e5c7b8dc0dd
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-07 01:24:38 +0300

    sSome changes

---
+++ Community/Product/Enums/CornerRadius.swift
@@ -0,0 +1,23 @@
+//
+//  CornerRadius.swift
+//  Community
+//
+//  Created by İsmail Ertuğlu on 4.12.2022.
+//
+
+import Foundation
+
+enum CornerRadius: Double {
+    
+    /// Return: 8
+    case small = 8.0
+    
+    /// Return: 16
+    case medium = 16.0
+
+    /// Return: 24
+    case large = 24.0
+    
+    /// Return: 50
+    case circle = 50.0
+}
